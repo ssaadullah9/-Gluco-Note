221250526794749cce5a485a898ab3267ade0cd8
Saja <ssaadullah9@gmail.com>
2022-03-29 01:14:39 +0300

    table done + reminder

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: primaryColor,
       ),
-      home: SplashScreen(),
+      home: ReminderScreeen(),
       // initialRoute: '/',
       //   routes: {
          //  '/': (context) => SplashScreen(),
